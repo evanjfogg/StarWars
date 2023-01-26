fc701c34ee58fd74498d4a40c0e1e88f08a5c118
evanjfogg <evanfogg@yahoo.com>
2023-01-26 16:27:37 -0600

    Added working Tabs, List, and Item components.

---
+++ src/app/item/item.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css']
+})
+export class ItemComponent {
+  @Input() character;
+  @Output() sideAssigned = new EventEmitter<{name: string, side: string}>();
+
+
+  onAssign(side) {
+    // this.character.side = side;
+    this.sideAssigned.emit({name: this.character.name, side: side});
+  }
+}
